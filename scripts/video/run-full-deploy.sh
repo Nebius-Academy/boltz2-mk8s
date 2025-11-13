ae9a5ca13d8cae12d0cf339f7058d4d2ac481b1c
dashabalashova <dashabalashova@gmail.com>
2025-11-13 12:44:50 +0100

    deploy

--- scripts/video/run-full-deploy.sh
+++ scripts/video/run-full-deploy.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# 1. MK8S cluster
+
+# 1.1 create registry, service account, MK8S cluster and node group
+
 git clone https://github.com/Nebius-Academy/boltz2-mk8s.git
 cd boltz2-mk8s
 
-# 1. docker build
-
-docker build -t boltz-runner -f docker/Dockerfile .
-
-# 2. MK8S cluster
-
-# 2.1 create registry, service account, MK8S cluster and node group
-
-# 2.2 export IDs
+# 1.2 set environment variables
 
 export PROJECT_ID=
 export REGION_ID=
@@ -21,7 +17,7 @@ export CLUSTER_ID=
 export MOUNT_TAG=
 export NB_REGISTRY_PATH=$(echo $NB_REGISTRY_ID | cut -d- -f2)
 
-# 2.3 command line interface (CLI) configuration
+# 1.3 configure Nebius CLI, docker login with short-lived access token, connect `kubectl` with the cluster
 
 nebius config set parent-id $PROJECT_ID
 
@@ -30,13 +26,29 @@ nebius iam get-access-token | \
     --username iam \
     --password-stdin
 
+nebius mk8s cluster get-credentials --id $CLUSTER_ID --external
+
+# 2. Docker container
+
+# 2.1 build -> tag/push -> pre-pull -> check status
+
+docker build -t boltz-runner -f docker/Dockerfile .
+
 export BOLTZ_IMAGE=cr.$REGION_ID.nebius.cloud/$NB_REGISTRY_PATH/boltz-runner:v1.0.0
 docker tag boltz-runner $BOLTZ_IMAGE
 docker push $BOLTZ_IMAGE
 
-# 3. Container Storage Interface PVC & data
+envsubst '$BOLTZ_IMAGE' < scripts/video/boltz-pre-pull.yaml | kubectl apply -f -
 
-nebius mk8s cluster get-credentials --id $CLUSTER_ID --external
+if kubectl rollout status daemonset/boltz-pre-pull --timeout=30m; then
+  echo "✅ COMPLETED"
+  kubectl delete daemonset/boltz-pre-pull
+else
+  echo "❌ FAILED"
+  exit 1
+fi
+
+# 3. Container Storage Interface PVC & data
 
 # 3.1 install CSI driver
 
@@ -55,30 +67,13 @@ kubectl apply -f scripts/video/csi-pvc-and-pod.yaml
 
 kubectl cp ./data/. my-csi-app:/data
 
-kubectl exec -it my-csi-app -- ls /data
-
-# 4. image and cache
-
-# 4.1 pull image and cache
-
-envsubst '$BOLTZ_IMAGE' < scripts/video/boltz-pre-pull.yaml | kubectl apply -f -
-
 kubectl apply -f scripts/video/boltz-cache-populate-job.yaml
 
-# 4.2 wait
-
 kubectl exec -it my-csi-app -- ls -lah /data/.boltz
-kubectl logs jobs/boltz-cache-populate
 
-if kubectl rollout status daemonset/boltz-pre-pull --timeout=20m; then
-  echo "✅ COMPLETED"
-  kubectl delete daemonset/boltz-pre-pull
-else
-  echo "❌ FAILED"
-  exit 1
-fi
+# 3.4 check status
 
-if kubectl wait --for=condition=complete job/boltz-cache-populate --timeout=20m; then
+if kubectl wait --for=condition=complete job/boltz-cache-populate --timeout=30m; then
   echo "✅ COMPLETED"
   kubectl delete job boltz-cache-populate --wait=false || true
   kubectl delete pods -l job-name=boltz-cache-populate || true
@@ -87,7 +82,9 @@ else
   exit 1
 fi
 
-# 5. run boltz and download results
+# 4. run boltz-2 and download results
+
+# 4.1 run boltz-2
 
 envsubst '$BOLTZ_IMAGE' < scripts/video/boltz-multi-job.yaml | kubectl apply -f -
 
@@ -99,5 +96,7 @@ echo "Waiting for boltz-runner job to complete..."
 kubectl wait --for=condition=complete job/boltz-runner --timeout=-1s
 completed=$(kubectl get pods -l job-name=boltz-runner --no-headers | grep 'Completed' | wc -l)
 echo "✅ COMPLETED: $completed/16 pods"
+
+# 4.2 download results
 
 kubectl cp my-csi-app:/data/results ./results -c my-csi-app
